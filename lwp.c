6f93a3043e1af707c6730a8579caad31aee7293d
Nolan Knievel <nolan.knievel@icloud.com>
2026-01-21 18:52:37 -0800

    update paths

--- lwp.c
+++ lwp.c
@@ -1,8 +1,7 @@
 #define next sched_one;
 #define prev sched_two;
-#include "~pn-cs453/Given/Asgn2/lwp.h"
-// #include "~pn-cs453/Given/Asgn2/lwp.h"
-// #include "~pn-cs453/Given/Asgn2/fp.h"
+#include "lwp.h";
+#include "fp.h";
 
 // RULES
 // saved bp/end of args must be divisible by 16
