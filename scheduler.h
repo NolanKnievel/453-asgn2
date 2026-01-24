5db99779b8690e6542e21ae82156e8e0ebb3a090
Nolan Knievel <nolan.knievel@icloud.com>
2026-01-23 19:22:45 -0800

    make scheduler funcs external

--- scheduler.h
+++ scheduler.h
@@ -7,8 +7,8 @@ void rr_admit(thread new);
 void rr_remove(thread victim);
 thread rr_next(void);
 int rr_qlen(void);
-void lwp_set_scheduler(scheduler sched);
-scheduler lwp_get_scheduler(void);
+extern void lwp_set_scheduler(scheduler sched);
+extern scheduler lwp_get_scheduler(void);
 
 // pre-defined RR scheduler
 extern struct scheduler rr_publish;
