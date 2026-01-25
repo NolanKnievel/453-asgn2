6924fd0d32e71164a81cdf519e361b85aae17bd6
Nolan Knievel <nolan.knievel@icloud.com>
2026-01-25 12:52:00 -0800

    quick push

--- test.c
+++ test.c
@@ -18,10 +18,10 @@ int main() {
     printf("creating thread\n");
 
     lwp_create((lwpfun)print_fun, NULL);
-    printf("thread created\n");
+    lwp_create(print_fun, NULL);
+    lwp_create(print_fun, NULL);
+    printf("threads created\n");
 
-    // lwp_create(print_fun, NULL);
-    // lwp_create(print_fun, NULL);
     printf("yielding to next thread\n");
     lwp_yield(); // yield control to next thread in schedule
     lwp_yield();
