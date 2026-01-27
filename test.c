e4211385f7625739485fe0a9c9aa1be9c4a26f85
Nolan Knievel <nolan.knievel@icloud.com>
2026-01-26 20:01:55 -0800

    tests

--- test.c
+++ test.c
@@ -7,17 +7,24 @@
 
 void print_fun(void *arg) {
     printf("hello from a thread!\n");
+    lwp_exit(5);
+    return;
+}
+
+void print_fun2(void *arg) {
+    printf("hello from a thread 2!\n");
     lwp_yield();
     lwp_exit(5);
     return;
 }
+
 
 void wait_fun(void *args) {
     printf("starting to wait\n");
     int status = -1;
     unsigned long exited_thread = lwp_wait(&status);
     printf("thread %lu exited with status %d\n", exited_thread, status);
-
+    lwp_exit();
 }
 
 int main() {
@@ -26,7 +33,7 @@ int main() {
 
     lwp_create((lwpfun)print_fun, NULL);
     lwp_create((lwpfun)wait_fun, NULL);
-    lwp_create((lwpfun)print_fun, NULL);
+    lwp_create((lwpfun)print_fun2, NULL);
 
     lwp_yield(); // yield control to next thread in schedule
     lwp_yield();
